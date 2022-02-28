78d977d5bfaec8d102184ce804cbc61f38c58729
jason112358 <jason112358@163.com>
2022-02-28 23:52:50 +0800

    Version 0.1

---
+++ get_img.py
@@ -0,0 +1,12 @@
+import requests
+from bs4 import BeautifulSoup
+
+bing_url = "https://www.bing.com"
+html_class = requests.get(bing_url)
+soup = BeautifulSoup(html_class.content, 'lxml')
+img_loc_soup = soup.select('#vs_cont > div.mc_caro > div.musCard > div.musCardCont > ul > li:nth-child(4) > a')
+img_loc = img_loc_soup[0]['href']   # 这里应该是列表结构，只有一个元素，元素内通过键值定位
+img_url = 'https://www.bing.com' + img_loc
+print(img_url)
+img_file = requests.get(img_url).content
+open('./bing20220228.jpg','wb').write(img_file)
