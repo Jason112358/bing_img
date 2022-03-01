c847f0f3fe06b8612140a18ee4b6a8912d51d23c
jason112358 <jason112358@163.com>
2022-03-01 22:15:38 +0800

    Version 0.2

--- README.md
+++ README.md
@@ -1,6 +1,5 @@
 # bing_img
 Download the everyday img on bing homepage.
-<<<<<<< HEAD
 
 ---
 ## PS:
@@ -9,5 +8,3 @@ This repository is uesd to learn Git & Github for myself.
 Of course, it can download some awesome pictures.
 
 **Appreciation for Bing**
-=======
->>>>>>> 35136920de3c774cc4640217b8c458fde203dde1
--- get_img.py
+++ get_img.py
@@ -1,11 +1,8 @@
 import requests
 from bs4 import BeautifulSoup
 import time
-<<<<<<< HEAD
 
 today = time.strftime("%Y-%m-%d", time.gmtime(time.time()+8*60*60))
-=======
->>>>>>> 35136920de3c774cc4640217b8c458fde203dde1
 
 bing_url = "https://www.bing.com"
 html_class = requests.get(bing_url)
@@ -14,15 +11,8 @@ soup = BeautifulSoup(html_class.content, 'lxml')
 
 img_loc_soup = soup.select('#vs_cont > div.mc_caro > div.musCard > div.musCardCont > ul > li:nth-child(4) > a')
 img_loc = img_loc_soup[0]['href']   # 这里应该是列表结构，只有一个元素，元素内通过键值定位
-<<<<<<< HEAD
 img_url = bing_url + img_loc
 
 print('Downloading...'+img_url)
 img_file = requests.get(img_url).content
 open('./img/bing-'+today+'.jpg','wb').write(img_file)
-=======
-img_url = 'https://www.bing.com' + img_loc
-print('Downloading...'+img_url)
-img_file = requests.get(img_url).content
-open('./bing-'+time.strftime("%Y-%m-%d", time.gmtime(time.time()+8*60*60))+'.jpg','wb').write(img_file)
->>>>>>> 35136920de3c774cc4640217b8c458fde203dde1
